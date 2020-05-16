1a58f75cfb5b2d6a8dd118b69b0017c994d34ee4
Zriyansh <52788043+zriyansh@users.noreply.github.com>
2020-05-16 23:01:52 +0530

    procfile added

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express")
-
+const path = require("path");
 var app = express();
-
+const port = process.env.PORT || 3000;
 app.set("view engine", "ejs")
 
 
@@ -10,6 +10,6 @@ app.get("/", function(req, res){
 })
 
 
-app.listen(3111, function(){
+app.listen(port, function(){
 	console.log("Starting port 3111 ");
 });
