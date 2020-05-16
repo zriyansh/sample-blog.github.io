c217010aa703ff8a7126445b0f1fbdec49b2c60c
Zriyansh <52788043+zriyansh@users.noreply.github.com>
2020-05-16 23:13:37 +0530

    procfile added

--- app.js
+++ app.js
@@ -2,11 +2,11 @@ var express = require("express")
 const path = require("path");
 var app = express();
 const port = process.env.PORT || 3000;
-app.set("view engine", "ejs")
+
 
 
 app.get("/", function(req, res){
-	res.render("home")
+	 res.sendFile(path.join(__dirname + '/home.html'))
 })
 
 
