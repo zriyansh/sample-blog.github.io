3b56468003d04e32b445f47f3b1f085e3e329b54
zriyansh <72187117+zriyansh@users.noreply.github.com>
2020-10-01 12:59:00 +0530

    Update app.js
    Added 4 routes to expand the project to multiple routes

--- app.js
+++ app.js
@@ -6,5 +6,20 @@ const port = process.env.PORT || 3000;
 app.get("/", function(req, res){
 	 res.sendFile(path.join(__dirname + '/index.html'))
 })
+app.get("/new", function(req, res){
+	 res.sendFile(path.join(__dirname + '/new.html'))
+})
+app.get("/delete", function(req, res){
+	 res.sendFile(path.join(__dirname + '/delete.html'))
+})
+app.get("/register", function(req, res){
+	 res.sendFile(path.join(__dirname + '/register.html'))
+})
+app.get("/login", function(req, res){
+	 res.sendFile(path.join(__dirname + '/login.html'))
+})
+
+
+
 
 app.listen(port, () => console.log(`sample-blog listening on port ${port}!`));
